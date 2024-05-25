53039798b4326d072fbe4d00787e14bc450fc9bd
Hyago Oliveira <hyagogow@gmail.com>
2024-05-25 17:32:01 -0300

    Add AddAreaToRight/Up functions

--- Runtime/Confiners/Confiner2DCollider.cs
+++ Runtime/Confiners/Confiner2DCollider.cs
@@ -15,12 +15,13 @@ namespace ActionCode.Cinemachine
 
         [Space]
         [SerializeField] private int index;
-        [SerializeField] private Rect area = new(0, 0, 30, 15);
+        [SerializeField] private Rect area = defaultArea;
 
         private static readonly Color outlineColor = new(0.1568628F, 0.9411765F, 0.1490196F);
         private static readonly Color faceColor = outlineColor * 0.16F;
 
         private int lastIndex = -1;
+        private static readonly Rect defaultArea = new(0, 0, 30, 15);
 
         private void Reset()
         {
@@ -50,8 +51,47 @@ namespace ActionCode.Cinemachine
 
         public PolygonCollider2D GetCollider() => collider;
 
+        [ContextMenu("Add Area To Right")]
+        public void AddAreaToRight() => AddArea(GetBottomRightPosition(index));
+
+        [ContextMenu("Add Area To Up")]
+        public void AddAreaToUp() => AddArea(GetTopLeftPosition(index));
+
+        public void AddArea(Vector2 position)
+        {
+            var area = defaultArea;
+            area.position = position;
+            TryAddNewPath(GetPathFrom(area));
+        }
+
+        public Vector2 GetBottomRightPosition(int index)
+        {
+            var area = GetRectFrom(collider.GetPath(index));
+            return new Vector2(area.xMax, area.yMin);
+        }
+
+        public Vector2 GetTopLeftPosition(int index)
+        {
+            var area = GetRectFrom(collider.GetPath(index));
+            return new Vector2(area.xMin, area.yMax);
+        }
+
         private void UpdatePathUsingIndex() => collider.SetPath(index, GetPathFrom(area));
         private void UpdateAreaUsingIndex() => area = GetRectFrom(collider.GetPath(index));
+
+        private void TryAddNewPath(Vector2[] path)
+        {
+            var newIndex = index + 1;
+            var isInvalidIndex = newIndex > collider.pathCount - 1;
+
+            if (isInvalidIndex)
+            {
+                Debug.LogError($"Cannot add new area since collider has only {collider.pathCount} Path Elements. Increase it manually.");
+                return;
+            }
+            // There isn't any collider.AddPath() and cannot possible to set collider.points property
+            collider.SetPath(newIndex, path);
+        }
 
         private void DrawEditRectangle()
         {
